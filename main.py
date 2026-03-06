305c570b9bc9692e9ab0458c787263132dff3112
aymanaghi <naghiayman@gmail.com>
2026-03-06 13:53:41 +0300

    Added CLI interface and OSINT modules

--- main.py
+++ main.py
@@ -1,29 +1,78 @@
-import sys
+import argparse
 import requests
+import whois
+from colorama import Fore, init
 
-sites = {
-    "GitHub": "https://github.com/{}",
-    "Reddit": "https://www.reddit.com/user/{}",
-    "Instagram": "https://www.instagram.com/{}",
-}
+init()
+
+banner = """
+   ____   _____ _____ _   _ ______ _____  
+  / __ \ / ____|_   _| \ | |  ____|  __ \ 
+ | |  | | (___   | | |  \| | |__  | |__) |
+ | |  | |\___ \  | | | . ` |  __| |  _  /
+ | |__| |____) |_| |_| |\  | |____| | \ \ 
+  \____/|_____/|_____|_| \_|______|_|  \_\
+
+        STINGER OSINT TOOLKIT
+"""
+
+print(Fore.CYAN + banner)
+
 
 def username_search(username):
-    print(f"\nSearching for username: {username}\n")
+    sites = {
+        "GitHub": "https://github.com/{}",
+        "Reddit": "https://www.reddit.com/user/{}",
+        "Instagram": "https://www.instagram.com/{}",
+    }
+
+    print(Fore.YELLOW + f"\nSearching username: {username}\n")
 
     for site, url in sites.items():
         r = requests.get(url.format(username))
 
         if r.status_code == 200:
-            print(f"[+] Found on {site}: {url.format(username)}")
+            print(Fore.GREEN + f"[+] Found on {site}: {url.format(username)}")
         else:
-            print(f"[-] Not found on {site}")
+            print(Fore.RED + f"[-] Not found on {site}")
 
-if len(sys.argv) < 3:
-    print("Usage: python main.py username <target>")
-    sys.exit()
 
-command = sys.argv[1]
-target = sys.argv[2]
+def ip_lookup(ip):
+    print(Fore.YELLOW + f"\nInvestigating IP: {ip}\n")
 
-if command == "username":
-    username_search(target)
+    r = requests.get(f"https://ipinfo.io/{ip}/json")
+    data = r.json()
+
+    for key, value in data.items():
+        print(Fore.GREEN + f"{key}: {value}")
+
+
+def domain_lookup(domain):
+    print(Fore.YELLOW + f"\nInvestigating domain: {domain}\n")
+
+    info = whois.whois(domain)
+
+    print(Fore.GREEN + f"Registrar: {info.registrar}")
+    print(Fore.GREEN + f"Creation Date: {info.creation_date}")
+    print(Fore.GREEN + f"Expiration Date: {info.expiration_date}")
+
+
+parser = argparse.ArgumentParser(description="OSINT Investigation Toolkit")
+
+parser.add_argument("--username", help="Search username across sites")
+parser.add_argument("--ip", help="Lookup IP information")
+parser.add_argument("--domain", help="Lookup domain WHOIS info")
+
+args = parser.parse_args()
+
+if args.username:
+    username_search(args.username)
+
+elif args.ip:
+    ip_lookup(args.ip)
+
+elif args.domain:
+    domain_lookup(args.domain)
+
+else:
+    print("Use --help for options")
